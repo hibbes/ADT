b5d70c4c0b7a46315a6e2d725a2b4938727541a6
MarekCzernohous <MarekCzernohous@0-208-Lehrer>
2022-06-28 15:37:57 +0200

    div

--- Listen/src/ListenElement.java
+++ Listen/src/ListenElement.java
@@ -15,47 +15,77 @@ public class ListenElement {
 		}
 	}
 
+	public ListenElement getNext() {
+		return next;
+	}
+
 	public int get() {
 		return inhalt;
 	}
-	
+
+	public void remove(int wert) {
+		if (next != null) {
+			if (next.get() == wert) {
+				if (next.getNext() != null) {
+					next = next.getNext();
+				} else {
+					next = null;
+				} 
+			}
+			else {
+				if(next!=null) {next.remove(wert);}
+			}
+		}
+		
+	}
+
+	public int find(int wert) {
+		if (inhalt == wert) {
+			return inhalt;
+		} else {
+			if (next == null) {
+				return -1;
+			} else {
+				return next.find(wert);
+			}
+		}
+
+	}
+
 	public void firstLast() {
 		if (next == null) {
 			System.out.print(inhalt);
 		} else {
 			next.firstLast();
 		}
-	
+
 	}
-	
+
 	public String toString() {
 		if (next == null) {
 			return "" + inhalt;
 		} else {
-			return ""+inhalt+" "+next.toString();
+			return "" + inhalt + " " + next.toString();
 		}
 	}
-	
-	
+
 	public int count() {
-		if(next == null) {
+		if (next == null) {
 			return 1;
-		}else {
-			return 1+next.count();
+		} else {
+			return 1 + next.count();
 		}
-			
-		}
-		
-		public int addition() {
-			if(next == null) {
-				return inhalt;
-			}else {
-				return inhalt+next.addition();
-				
-				
-			}
-		
+
 	}
-	
+
+	public int addition() {
+		if (next == null) {
+			return inhalt;
+		} else {
+			return inhalt + next.addition();
+
+		}
+
+	}
 
 }
