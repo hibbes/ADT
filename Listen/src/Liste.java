70d12bd2000021d1504384e47b38915f0a82a481
hibbes <marek@czernohous.de>
2022-06-29 07:32:33 +0200

    div

--- Listen/src/Liste.java
+++ Listen/src/Liste.java
@@ -1,36 +1,46 @@
- public class Liste {
+public class Liste {
 
-		private ListenElement kopf;
-		private ListenElement temp;
-	
-	public Liste (int wert) {
-		kopf=new ListenElement(wert);
+	private ListenElement kopf;
+	private ListenElement temp;
+
+	public Liste(int wert) {
+		kopf = new ListenElement(wert);
 	}
-	public void add (int wert) {
+
+	public void add(int wert) {
 		kopf.add(new ListenElement(wert));
 	}
-	
+
 	public void addHead(int wert) {
-		temp=kopf;
-		kopf= new ListenElement(wert);
+		temp = kopf;
+		kopf = new ListenElement(wert);
 		kopf.add(temp);
 	}
-	
+
+	public void remove(int wert) {
+		if (kopf.get() == wert) {
+			kopf = kopf.getNext();
+		}
+		kopf.remove(wert);
+	}
+
 	public void firstLast() {
-		System.out.print(kopf.get()+" ");
+		System.out.print(kopf.get() + " ");
 		kopf.firstLast();
 	}
-	
+
+	public int find(int wert) {
+		return kopf.find(wert);
+	}
+
 	public String toString() {
 		return kopf.toString();
 	}
-	
-	//Elemente zählen
-	
+
+	// Elemente zählen
+
 	public int counter() {
-	   return kopf.count();
+		return kopf.count();
 	}
-	
-	
-	
+
 }
--- Listen/src/Test.java
+++ Listen/src/Test.java
@@ -4,11 +4,12 @@ public class Test {
 	public static void main(String[] args) {
 		Liste liste = new Liste(3);
 		liste.add(5);
-		liste.add(7);
+		liste.add(43);
 		liste.add(9);
 		liste.add(12);
 		liste.add(13);
 		liste.add(7);
+		
 		System.out.println(liste);
 		
 		liste.addHead(42);
@@ -19,6 +20,11 @@ public class Test {
 		System.out.println(liste.counter());
 		
 		liste.firstLast();
+		
+		System.out.println("\n"+liste.find(4));
+		
+		liste.remove(7);
+		System.out.println(liste);
 		
 	}
 
