a53282a0baa759390cfd377d6ecd1bb7dd44fa0d
hibbes <marek@czernohous.de>
2022-07-15 09:24:14 +0200

    Refactor

--- Listen/src/Test.java
+++ Listen/src/Test.java
@@ -1,38 +1,68 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Test {
+	static long startTime = 0;
+	static int length = 10;
+	static long endTime = 0;
 
 	public static void main(String[] args) {
-		QuickSort q = new QuickSort();
-	    int[] zuSortieren = {3,6,1,9,7,1,6};
-	    System.out.println(Arrays.toString(zuSortieren));
-	    q.quicksort(zuSortieren,  0, 6);
-	    System.out.println(Arrays.toString(zuSortieren));
-		//
-	/*	Liste liste = new Liste(3);
-		liste.add(5);
-		liste.add(43);
-		liste.add(9);
-		liste.add(12);
-		liste.add(13);
-		liste.add(7);
-		
-		System.out.println(liste);
-		
-		liste.addHead(42);
-		
-		System.out.println(liste);
-		
-		
-		System.out.println(liste.counter());
-		
-		liste.firstLast();
-		
-		System.out.println("\n"+liste.find(4));
-		
-		liste.remove(7);
-		System.out.println(liste);
-		*/
-	} 
 
-} 
+		// Array generieren
+
+		Random rand = new Random();
+		int[] numbers = new int[length];
+		int[] toSort = new int[length];
+
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = rand.nextInt(1000000);
+		}
+
+		QuickSort q = new QuickSort();
+
+		// Quicksort
+
+		System.arraycopy(numbers, 0, toSort, 0, numbers.length);
+		System.out.println("QuickSort");
+		startTime = System.currentTimeMillis();
+		q.quicksort(toSort, 0, toSort.length - 1);
+		endTime = System.currentTimeMillis();
+		System.out.print("Zeit: ");
+		System.out.println(endTime-startTime);
+
+		System.out.println(Arrays.toString(toSort));
+
+		// SelectionSort
+		System.arraycopy(numbers, 0, toSort, 0, numbers.length);
+		System.out.println("SelectionSort");
+		System.out.println(Arrays.toString(SortAlgorithms.selectionSort(toSort)));
+
+		// BubbleSort
+		System.arraycopy(numbers, 0, toSort, 0, numbers.length);
+		System.out.println("BubbleSort");
+		System.out.println(Arrays.toString(SortAlgorithms.bubbleSort(toSort)));
+
+	}
+
+}
+
+//
+/*
+ * Liste liste = new Liste(3); liste.add(5); liste.add(43); liste.add(9);
+ * liste.add(12); liste.add(13); liste.add(7);
+ * 
+ * System.out.println(liste);
+ * 
+ * liste.addHead(42);
+ * 
+ * System.out.println(liste);
+ * 
+ * 
+ * System.out.println(liste.counter());
+ * 
+ * liste.firstLast();
+ * 
+ * System.out.println("\n"+liste.find(4));
+ * 
+ * liste.remove(7); System.out.println(liste);
+ */
